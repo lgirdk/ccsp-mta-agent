485504b1406bfbe4fe040cb977089fd99de472fb
George Milner <george.milner@consult.red>
2020-08-17 16:13:11 +0000

    add LGI TR-181 MTA Data Model implementation
    Adds the following objects:
    
      Device.X_LGI-COM_MTA.PacketCableRegion
      Device.X_LGI-COM_MTA.LineInfoNumberOfEntries
      Device.X_LGI-COM_MTA.LineInfo.{i}.OperStatus
      Device.X_LGI-COM_MTA.LineInfo.{i}.AdminStatus
      Device.X_LGI-COM_MTA.LineTableNumberOfEntries
      Device.X_LGI-COM_MTA.LineTable.{i}.Status

---
+++ source/TR-181/include/cosa_x_lgi_com_mta_apis.h
@@ -0,0 +1,64 @@
+#ifndef  _COSA_LGI_MTA_APIS_H
+#define  _COSA_LGI_MTA_APIS_H
+
+#include "cosa_apis.h"
+#include "plugin_main_apis.h"
+
+/**********************************************************************
+                STRUCTURE AND CONSTANT DEFINITIONS
+**********************************************************************/
+
+typedef  struct
+_COSA_LGI_MTA_LINEINFO
+{
+    ULONG                           InstanceNumber;
+    ULONG                           OperStatus;
+    ULONG                           AdminStatus;
+    ULONG                           HookStatus;
+}
+COSA_LGI_MTA_LINEINFO, *PCOSA_LGI_MTA_LINEINFO;
+
+
+/**********************************************************************
+                FUNCTION PROTOTYPES
+**********************************************************************/
+
+ANSC_STATUS
+CosaDmlLgiMTAInit
+    (
+        ANSC_HANDLE                 hDml,
+        PANSC_HANDLE                phContext
+    );
+
+ANSC_STATUS
+CosaDmlMTAGetPacketCableRegion
+    (
+        ANSC_HANDLE                 hContext,
+        ULONG                       *pPacketCableRegion
+    );
+
+ULONG
+CosaDmlMTALineInfoGetNumberOfEntries
+    (
+        ANSC_HANDLE                 hContext
+    );
+
+ANSC_STATUS
+CosaDmlMTALineInfoGetEntry
+    (
+        ANSC_HANDLE                 hContext,
+        ULONG                       ulIndex,
+        PCOSA_LGI_MTA_LINEINFO      pEntry
+    );
+
+ANSC_STATUS
+CosaDmlMTALineInfoSetAdminStatus
+    (
+        ANSC_HANDLE                 hContext,
+        ULONG                       ulIndex,
+        ULONG                       ulValue
+    );
+
+
+#endif /* _COSA_LGI_MTA_APIS_H */
+
