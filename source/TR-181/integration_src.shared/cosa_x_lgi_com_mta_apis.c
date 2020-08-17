2f283a63182483be3b4882e7e6142cacc56cd77c
George Milner <george.milner@consult.red>
2020-08-17 16:13:11 +0000

    add LGI TR-181 MTA Data Model implementation
    Adds the following objects:
    
      Device.X_LGI-COM_MTA.PacketCableRegion
      Device.X_LGI-COM_MTA.LineInfoNumberOfEntries
      Device.X_LGI-COM_MTA.LineInfo.{i}.OperStatus
      Device.X_LGI-COM_MTA.LineInfo.{i}.AdminStatus
      Device.X_LGI-COM_MTA.LineTableNumberOfEntries
      Device.X_LGI-COM_MTA.LineTable.{i}.Status
    
    add support for X_LGI-COM_MTA.SpeedUpMTA-RFLossDetection ( OFW-2014 )
    
    FIX DESCRIPTION:
    
    Implemented the below TR-181 Data Model parameter.
    
    Device.X_LGI-COM_MTA.SpeedUpMTA-RFLossDetection
    
    TESTING PERFORMED:
    
    1. Bring up the DUT online.
    2. Perform GPV/SPV for the parameters via dmcli
    3. Perform GPV/SPV for the parameters via tr069
    
    Signed-off-by: Saravanakumar Karthikeyan <skarthikeyan.contractor@libertyglobal.com>

---
+++ source/TR-181/integration_src.shared/cosa_x_lgi_com_mta_apis.c
@@ -0,0 +1,60 @@
+//!!!  This code assumes that all data structures are the SAME in middle-layer APIs and HAL layer APIs
+//!!!  So it uses casting from one to the other
+#include "cosa_x_lgi_com_mta_apis.h"
+#include "mta_hal.h"
+#include <syscfg/syscfg.h>
+
+ANSC_STATUS CosaDmlLgiMTAInit (ANSC_HANDLE hDml, PANSC_HANDLE phContext)
+{
+    if (mta_hal_InitDB() != RETURN_OK)
+        return ANSC_STATUS_FAILURE;
+
+    return ANSC_STATUS_SUCCESS;
+}
+
+ANSC_STATUS CosaDmlMTAGetPacketCableRegion (ANSC_HANDLE hContext, ULONG *pPacketCableRegion)
+{
+    if (mta_hal_GetPacketCableRegion(pPacketCableRegion) != RETURN_OK)
+        return ANSC_STATUS_FAILURE;
+
+    return ANSC_STATUS_SUCCESS;
+}
+
+ULONG CosaDmlMTALineInfoGetNumberOfEntries (ANSC_HANDLE hContext)
+{
+    return mta_hal_LineInfoGetNumberOfEntries();
+}
+
+ANSC_STATUS CosaDmlMTALineInfoGetEntry (ANSC_HANDLE hContext, ULONG ulIndex, PCOSA_LGI_MTA_LINEINFO pEntry)
+{
+    if (mta_hal_LineInfoGetEntry(ulIndex, (PMTAMGMT_MTA_LINETABLE_INFO)pEntry) != RETURN_OK)
+        return ANSC_STATUS_FAILURE;
+
+    return ANSC_STATUS_SUCCESS;
+}
+
+ANSC_STATUS CosaDmlMTALineInfoSetAdminStatus (ANSC_HANDLE hContext, ULONG ulIndex, ULONG ulValue)
+{
+    if (mta_hal_LineInfoSetAdminStatus(ulIndex, ulValue) != RETURN_OK)
+        return ANSC_STATUS_FAILURE;
+
+    return ANSC_STATUS_SUCCESS;
+}
+
+BOOL CosaDmlMTABasicInfoGetSpeedUpMTARFLossDetection (ANSC_HANDLE hContext)
+{
+    char buf[8];
+    BOOL value;
+
+    syscfg_get(NULL, "mta_rf_loss_detection", buf, sizeof(buf));
+    value = (strcmp(buf, "1") == 0);
+
+    return value;
+}
+
+int CosaDmlMTABasicInfoSetSpeedUpMTARFLossDetection (BOOL bValue)
+{
+    syscfg_set_commit(NULL, "mta_rf_loss_detection", bValue ? "1" : "0");
+
+    return platform_hal_SetSpeedUpMTARFLossDetection(bValue);
+}
